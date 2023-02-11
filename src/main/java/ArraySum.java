953497e9120f86204a57e6cdbedb590a8c3d8075
Joshua David Gardner <jgardn11@yahoo.com>
2023-02-11 15:05:25 +0000

    Complete

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,9 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int arrSum = 0;
+        for(int i = 0; i < arr.length; i++)
+            arrSum += arr[i];
+        return arrSum;
     }
 }
